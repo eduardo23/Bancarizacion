10c3e73d30f0d0a63de80c50705486e182b5fce2
Hermes <eduardo23@gmail.com>
2018-06-29 22:00:06 -0500

    ads

---
+++ DAO_Hermes/ViewModel/LogPromoDet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO_Hermes.ViewModel
+{
+    public class LogPromoDet
+    {
+        private int _ID;
+        private int _logPromoID;
+        private int id_grupo_correo;
+        private string _destinatario;
+
+        public int ID {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+
+    }
+}
